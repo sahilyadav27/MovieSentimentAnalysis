689a66b3f36afbdd0496886dc8485c65abd74154
Sahil Yadav <53268444+sahilyadav27@users.noreply.github.com>
2019-08-09 19:36:11 +0530

    Add files via upload

---
+++ SentimentAnalyzer.py
@@ -0,0 +1,87 @@
+from importlib import reload
+import sys
+import pandas as pd
+
+import re
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+
+df1 = pd.read_csv('/home/shlydv/Downloads/Data.tsv', delimiter="\t")
+df1 = df1.drop(['id'], axis=1)
+df2 = pd.read_csv('/home/shlydv/Downloads/IMDbData.csv',encoding="latin-1")
+df2 = df2.drop(['Unnamed: 0','type','file'],axis=1)
+df2.columns = ["review","sentiment"]
+df2 = df2[df2.sentiment != 'unsup']
+df2['sentiment'] = df2['sentiment'].map({'pos': 1, 'neg': 0})
+df = pd.concat([df1, df2]).reset_index(drop=True)
+
+
+
+
+import re
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+stop_words = set(stopwords.words("english")) 
+lemmatizer = WordNetLemmatizer()
+
+
+def clean_text(text):
+    text = re.sub(r'[^\w\s]','',text, re.UNICODE)
+    text = text.lower()
+    text = [lemmatizer.lemmatize(token) for token in text.split(" ")]
+    text = [lemmatizer.lemmatize(token, "v") for token in text]
+    text = [word for word in text if not word in stop_words]
+    text = " ".join(text)
+    return text
+
+df['review'] = df.review.apply(lambda x: clean_text(x))
+X_train, X_test, y_train, y_test = train_test_split(df['review'], df['sentiment'], random_state=1)
+
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Dense , Input , LSTM , Embedding, Dropout , Activation, GRU, Flatten
+from keras.layers import Bidirectional, GlobalMaxPool1D
+from keras.models import Model, Sequential
+from keras.layers import Convolution1D
+from keras import initializers, regularizers, constraints, optimizers, layers
+
+max_features = 6000
+tokenizer = Tokenizer(num_words=max_features)
+tokenizer.fit_on_texts(X_train)
+list_tokenized_train = tokenizer.texts_to_sequences(X_train)
+
+maxlen = 130
+X_t = pad_sequences(list_tokenized_train, maxlen=maxlen)
+y = y_train
+
+embed_size = 128
+model = Sequential()
+model.add(Embedding(max_features, embed_size))
+model.add(Bidirectional(LSTM(32, return_sequences = True)))
+model.add(GlobalMaxPool1D())
+model.add(Dense(20, activation="relu"))
+model.add(Dropout(0.05))
+model.add(Dense(1, activation="sigmoid"))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+batch_size = 100
+epochs = 3
+model.fit(X_t,y, batch_size=batch_size, epochs=epochs, validation_split=0.2)
+
+
+
+
+list_sentences_test = X_test
+list_tokenized_test = tokenizer.texts_to_sequences(list_sentences_test)
+X_te = pad_sequences(list_tokenized_test, maxlen=maxlen)
+prediction = model.predict(X_te)
+y_pred = (prediction > 0.5)
+
+from sklearn.metrics import f1_score, confusion_matrix
+
+print('F1-score: {0}'.format(f1_score(y_pred, y_test)))
+print('Confusion matrix:')
+confusion_matrix(y_pred, y_test)
