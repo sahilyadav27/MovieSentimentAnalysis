70ba2a257df1fba1d9be19f56f49c643dd05976b
Sahil Yadav <53268444+sahilyadav27@users.noreply.github.com>
2019-08-09 22:57:05 +0530

    Update SentimentAnalyzer.py

--- SentimentAnalyzer.py
+++ SentimentAnalyzer.py
@@ -8,7 +8,7 @@ from nltk.corpus import stopwords
 
 #Adding datasets
 
-df1 = pd.read_csv('/home/shlydv/Downloads/Data.tsv', delimiter="\t")
+df1 = pd.read_csv('/home/shlydv/Downloads/TrainData.tsv', delimiter="\t")
 df1 = df1.drop(['id'], axis=1)
 df2 = pd.read_csv('/home/shlydv/Downloads/IMDbData.csv',encoding="latin-1")
 df2 = df2.drop(['Unnamed: 0','type','file'],axis=1)
@@ -39,13 +39,10 @@ def clean_text(text):
     text = " ".join(text)
     return text
 
-df['review'] = df.review.apply(lambda x: clean_text(x))
-
-#Splitting the dataset into test and train
-X_train, X_test, y_train, y_test = train_test_split(df['review'], df['sentiment'], random_state=1)
+df['Processed_Reviews'] = df.review.apply(lambda x: clean_text(x))
 
 
-from keras.preprocessing.text import Tokenizer
+ffrom keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.layers import Dense , Input , LSTM , Embedding, Dropout , Activation, GRU, Flatten
 from keras.layers import Bidirectional, GlobalMaxPool1D
@@ -53,16 +50,14 @@ from keras.models import Model, Sequential
 from keras.layers import Convolution1D
 from keras import initializers, regularizers, constraints, optimizers, layers
 
-#Adding layers using Keras
-
 max_features = 6000
 tokenizer = Tokenizer(num_words=max_features)
-tokenizer.fit_on_texts(X_train)
-list_tokenized_train = tokenizer.texts_to_sequences(X_train)
+tokenizer.fit_on_texts(df['Processed_Reviews'])
+list_tokenized_train = tokenizer.texts_to_sequences(df['Processed_Reviews'])
 
 maxlen = 130
 X_t = pad_sequences(list_tokenized_train, maxlen=maxlen)
-y = y_train
+y = df['sentiment']
 
 embed_size = 128
 model = Sequential()
@@ -74,20 +69,21 @@ model.add(Dropout(0.05))
 model.add(Dense(1, activation="sigmoid"))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-#Fitting the model on the data
-
 batch_size = 100
 epochs = 3
 model.fit(X_t,y, batch_size=batch_size, epochs=epochs, validation_split=0.2)
 
-list_sentences_test = X_test
+df_test=pd.read_csv("/home/shlydv/Downloads/TestData.tsv",header=0, delimiter="\t", quoting=3)
+df_test.head()
+df_test["review"]=df_test.review.apply(lambda x: clean_text(x))
+df_test["sentiment"] = df_test["id"].map(lambda x: 1 if int(x.strip('"').split("_")[1]) >= 5 else 0)
+y_test = df_test["sentiment"]
+list_sentences_test = df_test["review"]
 list_tokenized_test = tokenizer.texts_to_sequences(list_sentences_test)
 X_te = pad_sequences(list_tokenized_test, maxlen=maxlen)
 prediction = model.predict(X_te)
 y_pred = (prediction > 0.5)
-
 from sklearn.metrics import f1_score, confusion_matrix
-
 print('F1-score: {0}'.format(f1_score(y_pred, y_test)))
 print('Confusion matrix:')
 confusion_matrix(y_pred, y_test)
